233ed25060705a81b969b8f1e296a70fad72456b
Erik Bäckdahl <erik.backdahl@gmail.com>
2025-05-19 19:19:46 +0200

    token hidden

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    private static DiscordSocketClient _client;
+
+    public static async Task Main()
+    {
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        
+        
+        var token = File.ReadAllText(@"C:\Users\Erik\VSC\PROJECTS\DiscordBotTest\token\token.txt");
+        
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
+
