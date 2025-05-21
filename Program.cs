9b4080abd083be1714f8a80e98261386a727e43e
Erik Bäckdahl <erik.backdahl@gmail.com>
2025-05-21 14:59:24 +0200

    adding commands

--- Program.cs
+++ Program.cs
@@ -3,30 +3,90 @@ using System.IO;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Net;
+using Newtonsoft.Json;
 
 public class Program
 {
-    private static DiscordSocketClient _client;
+    private static DiscordSocketClient client;
 
     public static async Task Main()
     {
-        _client = new DiscordSocketClient();
+        client = new DiscordSocketClient();
 
-        _client.Log += Log;
+        client.Log += Log;
 
         //  You can assign your bot token to a string, and pass that in to connect.
         //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-        
-        
-        var token = File.ReadAllText(@"C:\Users\Erik\VSC\PROJECTS\DiscordBotTest\token\token.txt");
-        
 
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
+
+        var token = File.ReadAllText(@"C:\Users\Erik\VSC\PROJECTS\DiscordBotTest\token\token.txt");
+
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        client.Ready += Client_Ready;
+
+        client.SlashCommandExecuted += SlashCommandHandler;
+
+
+        while (true)
+        {
+
+        }
+
 
         // Block this task until the program is closed.
-        await Task.Delay(-1);
+        // await Task.Delay(-1);
+
+
     }
+
+    public static async Task Client_Ready()
+    {
+        // Let's build a guild command! We're going to need a guild so lets just put that in a variable.
+        //var guild = client.GetGuild(guildId);
+
+        // Next, lets create our slash command builder. This is like the embed builder but for slash commands.
+        var guildCommand = new SlashCommandBuilder();
+
+        // Note: Names have to be all lowercase and match the regular expression ^[\w-]{3,32}$
+        guildCommand.WithName("first-command");
+
+        // Descriptions can have a max length of 100.
+        guildCommand.WithDescription("This is my first guild slash command!");
+
+        // Let's do our global command
+        var globalCommand = new SlashCommandBuilder();
+        globalCommand.WithName("first-global-command");
+        globalCommand.WithDescription("This is my first global slash command");
+
+        try
+        {
+            // Now that we have our builder, we can call the CreateApplicationCommandAsync method to make our slash command.
+            //await guild.CreateApplicationCommandAsync(guildCommand.Build());
+
+            // With global commands we don't need the guild.
+            await client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+            // Using the ready event is a simple implementation for the sake of the example. Suitable for testing and development.
+            // For a production bot, it is recommended to only run the CreateGlobalApplicationCommandAsync() once for each command.
+        }
+        catch (HttpException exception)
+        {
+            // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+            var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+
+            // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
+            Console.WriteLine(json);
+        }
+    }
+    private static async Task SlashCommandHandler(SocketSlashCommand command)
+    {
+        await command.RespondAsync($"You executed {command.Data.Name}");
+    }
+
     private static Task Log(LogMessage msg)
     {
         Console.WriteLine(msg.ToString());
